d7a7ed8e7861191e36ea90624059102d3b4be9fe
lokesh <kottelokesh789@gmail.com>
2022-01-28 18:14:49 +0530

    ADDED: RetrieveDataFromTheRange

--- src/main/java/com/bridgelabz/addressbook_jdbc/AddressBookDBService.java
+++ src/main/java/com/bridgelabz/addressbook_jdbc/AddressBookDBService.java
@@ -1,14 +1,35 @@
 package com.bridgelabz.addressbook_jdbc;
 
 import java.beans.Statement;
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBookDBService {
+	private PreparedStatement addressBookPreparedStatement;
 	private static AddressBookDBService addressBookDBService;
+	private List<AddressBookData> addressBookData;
+
+	private AddressBookDBService() {
+	}
+
+	private Connection getConnection() throws SQLException {
+		String jdbcURL = "jdbc:mysql://localhost:3306/address_book_system?useSSL=false";
+		String username = "root";
+		String password = "******";
+		Connection con;
+		System.out.println("Connecting to database ");
+		con = DriverManager.getConnection(jdbcURL, username, password);
+		System.out.println("Connection is successful ");
+		return con;
+
+	}
 
 	public static AddressBookDBService getInstance() {
 		if (addressBookDBService == null)
@@ -17,24 +38,35 @@ public class AddressBookDBService {
 	}
 
 	public List<AddressBookData> readData() throws AddressBookException {
-		String sql = "SELECT * FROM addressBook; ";
-		return this.getAddressBookDataUsingDB(sql);
+		String query;
+		query = "select * from addressBook";
+		return getAddressBookDataUsingDB(query);
 	}
 
 	private List<AddressBookData> getAddressBookDataUsingDB(String sql) throws AddressBookException {
-		List<AddressBookData> addressBookList = new ArrayList<>();
-		try (Connection connection = AddressBookConnection.getConnection();) {
+		List<AddressBookData> addressBookData = new ArrayList<>();
+		try (Connection connection = this.getConnection()) {
 			Statement statement = (Statement) connection.createStatement();
 			ResultSet resultSet = ((java.sql.Statement) statement).executeQuery(sql);
-			addressBookList = this.getAddressBookData(resultSet);
+			addressBookData = this.getAddressBookDetails(resultSet);
 		} catch (SQLException e) {
 			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
 		}
-		return addressBookList;
+		return addressBookData;
 	}
 
-	private List<AddressBookData> getAddressBookData(ResultSet resultSet) throws AddressBookException {
-		List<AddressBookData> addressBookList = new ArrayList<>();
+	private void prepareAddressBookStatement() throws AddressBookException {
+		try {
+			Connection connection = this.getConnection();
+			String query = "select * from addressBook where FirstName = ?";
+			addressBookPreparedStatement = connection.prepareStatement(query);
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
+		}
+	}
+
+	private List<AddressBookData> getAddressBookDetails(ResultSet resultSet) throws AddressBookException {
+		List<AddressBookData> addressBookData = new ArrayList<>();
 		try {
 			while (resultSet.next()) {
 				String firstName = resultSet.getString("FirstName");
@@ -42,12 +74,54 @@ public class AddressBookDBService {
 				String address = resultSet.getString("Address");
 				String city = resultSet.getString("City");
 				String state = resultSet.getString("State");
-				java.math.BigDecimal zip = resultSet.getBigDecimal("Zip");
-				java.math.BigDecimal phoneNo = resultSet.getBigDecimal("PhoneNumber");
+				BigDecimal zip = resultSet.getBigDecimal("Zip");
+				BigDecimal phoneNo = resultSet.getBigDecimal("PhoneNumber");
 				String email = resultSet.getString("Email");
-				addressBookList
+				addressBookData
 						.add(new AddressBookData(firstName, lastName, address, city, state, zip, phoneNo, email));
 			}
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
+		}
+		return addressBookData;
+	}
+
+	public int updateAddressBookData(String firstname, String address) throws AddressBookException {
+		String query = String.format("update addressBook set Address = '%s' where FirstName = '%s';", address,
+				firstname);
+		try (Connection connection = this.getConnection()) {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			return preparedStatement.executeUpdate(query);
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
+		}
+	}
+
+	public List<AddressBookData> getAddressBookData(String firstname) throws AddressBookException {
+		if (this.addressBookPreparedStatement == null)
+			this.prepareAddressBookStatement();
+		try {
+			addressBookPreparedStatement.setString(1, firstname);
+			ResultSet resultSet = addressBookPreparedStatement.executeQuery();
+			addressBookData = this.getAddressBookDetails(resultSet);
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
+		}
+		System.out.println(addressBookData);
+		return addressBookData;
+	}
+
+	public List<AddressBookData> readData(LocalDate start, LocalDate end) throws AddressBookException {
+		String query = null;
+		if (start != null)
+			query = String.format("select * from addressBook where Date between '%s' and '%s';", start, end);
+		if (start == null)
+			query = "select * from addressBook";
+		List<AddressBookData> addressBookList = new ArrayList<>();
+		try (Connection con = this.getConnection();) {
+			Statement statement = (Statement) con.createStatement();
+			ResultSet rs = ((java.sql.Statement) statement).executeQuery(query);
+			addressBookList = this.getAddressBookDetails(rs);
 		} catch (SQLException e) {
 			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
 		}
--- src/main/java/com/bridgelabz/addressbook_jdbc/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbook_jdbc/AddressBookService.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook_jdbc;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookService {
@@ -14,14 +15,45 @@ public class AddressBookService {
 		addressBookDBService = AddressBookDBService.getInstance();
 	}
 
-	public AddressBookService(List<AddressBookData> addresBookList) {
-		this();
-		this.addressBookList = addressBookList;
-	}
-
 	public List<AddressBookData> readAddressBookData(IOService ioservice) throws AddressBookException {
 		if (ioservice.equals(IOService.DB_IO))
-			this.addressBookList = addressBookDBService.readData();
+			return this.addressBookList = addressBookDBService.readData();
 		return this.addressBookList;
+	}
+
+	public void updateRecord(String firstname, String address) throws AddressBookException {
+		int result = addressBookDBService.updateAddressBookData(firstname, address);
+		if (result == 0)
+			return;
+		AddressBookData addressBookData = this.getAddressBookData(firstname);
+		if (addressBookData != null)
+			addressBookData.address = address;
+	}
+
+	public boolean checkUpdatedRecordSyncWithDatabase(String firstname) throws AddressBookException {
+		try {
+			List<AddressBookData> addressBookData = addressBookDBService.getAddressBookData(firstname);
+			return addressBookData.get(0).equals(getAddressBookData(firstname));
+		} catch (AddressBookException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
+		}
+	}
+
+	private AddressBookData getAddressBookData(String firstname) {
+		return this.addressBookList.stream().filter(addressBookItem -> addressBookItem.firstname.equals(firstname))
+				.findFirst().orElse(null);
+	}
+
+	public List<AddressBookData> readAddressBookData(IOService ioService, String start, String end)
+			throws AddressBookException {
+		try {
+			LocalDate startLocalDate = LocalDate.parse(start);
+			LocalDate endLocalDate = LocalDate.parse(end);
+			if (ioService.equals(IOService.DB_IO))
+				return addressBookDBService.readData(startLocalDate, endLocalDate);
+			return this.addressBookList;
+		} catch (AddressBookException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DATABASE_EXCEPTION);
+		}
 	}
 }
